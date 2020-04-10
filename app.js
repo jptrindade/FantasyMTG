e85b02967e20328b32c2213576a7190f72bf0364
jptrindade <joao_pedro_trindade@hotmail.com>
2020-04-10 15:26:12 +0100

    chaged port to env

--- app.js
+++ app.js
@@ -26,4 +26,4 @@ mongoose.connect(
     process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true } ,
      () => console.log('connected to MongoDB Atlas'))
 
-app.listen(3000);
+app.listen(process.env.PORT);
